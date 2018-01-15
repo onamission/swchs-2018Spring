f279cbbd33391e394a16a69a2faa505028a070e8
Tim Turnquist <tim.turnquist@gmail.com>
2018-01-15 14:47:36 -0600

    Split out Deal funcitons

--- GamePlay.js
+++ GamePlay.js
@@ -13,9 +13,8 @@ class GamePlay{
         var playerSelector = $("#p")[0];
         var playerCount =  playerSelector.options[ playerSelector.selectedIndex ].value;
         var round = 1;
-        var cardCount = 0;
 
-        // put the data on the page for easy access
+        // put some of the data on the page for easy access
         $("#gData").text( JSON.stringify( gameData ) );
         $("#deck").text( JSON.stringify( shuffledDeck ) );
 
@@ -31,11 +30,12 @@ class GamePlay{
         }
 
         // set up next round button
+        var cardCount = {player : 0, community: 0, cards_already_dealt : 0};
+        var parameters = round + ',' + JSON.stringify( cardCount )
         if( $("#dealDiv #play_button") && $("#dealDiv #play_button").length ){
-            $("#play_button").attr( "onclick", "gamePlay.dealACard( 1, 0," + playerCount + ");" ).val("Deal Round " + round)
+            $("#play_button").attr(  "onclick", 'gamePlay.dealACard( ' + parameters + ' );' ).val( "Deal Round " + ( round ) );
         }else{
-            $("#dealDiv").append( "<input id=\"play_button\" type=\"button\" onClick=\"gamePlay.dealACard( " +
-                round + ", " + cardCount + " )\" value=\"Deal Round " + round + "\">");
+            $("#dealDiv").append( "<input id=\"play_button\" type=\"button\" onClick='gamePlay.dealACard( " + parameters + " )' value=\"Deal Round " + round + "\">");
         }
 
         // Set the background image based on base64 data
@@ -69,7 +69,7 @@ class GamePlay{
         }
         if( communityCards ){
             for( var p = 0; p < players; p++){
-                for(var  com = playerCards; com < communityCards; com++){
+                for(var  com = playerCards; com < ( communityCards + playerCards ); com++){
                     if( !hand[p] ){
                         hand[p] = [];
                     }
@@ -94,7 +94,7 @@ class GamePlay{
         };
 
         var playerNumber = $(".hand") ? $(".hand").length : 1;
-        $("#play_button").attr( "onclick", "gamePlay.dealACard( 1, 0," + playerNumber + ");" ).val( "Start New Game" );
+        $("#play_button").attr( "onclick", "gamePlay.dealACard( 1, {\"player\" : 0, \"community\": 0, \"cards_already_dealt\": 0} );" ).val( "Start New Game" );
         // reshuffle the deck
         var newDeck = new Deck();
         var shuffledDeck = newDeck.shuffleTheDeck( ( $("input[name=d]:checked" ).val() || 1 ) );
@@ -104,8 +104,8 @@ class GamePlay{
     /**
     * Deal the cards
     */
-   dealACard( round, cardsAlreadyDealt ){
-       if( round === 1 && cardsAlreadyDealt == 0 ){
+   dealACard( round, cardCount ){
+       if( round === 1 && cardCount.cards_already_dealt == 0 ){
             // this is a new hand, so reset all of the players cards
             $(".back").css( { transform: "rotateY(0deg)" }) ;
             // clear score
@@ -117,118 +117,37 @@ class GamePlay{
        var cardsToDealThisRound = gameData.dealOrder[round].cardsToDeal;
        var dealToWhoThisRound = gameData.dealOrder[round].dealTo;
        var howToDealThisRound = gameData.dealOrder[round].cardDirection;
-       var communityCardCount = 0;
-       var playerCardCount = 0;
        if( gameData.name == "5draw" && round == 1 ){
            $("#msg").html( "Click on up to 3 cards to replace. Once you have your cards selected, click on \"Deal Round 2\" to replace those cards");
        }
-       if( dealToWhoThisRound == "replace" ){
-           var sel = $(".selected > div.card.front");
-           // var sel = $(".selected .front");
-           if( sel.length > 3 ){
-               alert( "You can only select three cards to replace." );
-               round--; //reset the round
-           }else{
-               // first we must change the cards
-               sel.each( s =>{
-                   var crd = remainingDeck.shift();
-                   var f = sel[s];
-                   f.innerHTML = crd.cardValue ;
-                   f.setAttribute("data-ordr",crd.card.order);
-                   f.setAttribute("data-suit",crd.suit.name);
-                   f.setAttribute("data-value",crd.card.value);
-                   f.style.color=crd.suit.color;
-                   toggleClass( f.parentElement.id, "selected" );
-               });
-           }
-       }
-       if( dealToWhoThisRound == "players"){
-           for( var p = 0; p < numberOfPlayers; p++){
-               // find the first unplayed card in the hand
-               var nextCardToPlay = this.findFirstUnplayedCard( p, cardsAlreadyDealt, cardsToDealThisRound );
-               for( var c = nextCardToPlay; c < cardsToDealThisRound; c++ ){
-                   var f = $("#p" + p + "c" + c + " div.front");
-                   var b = $("#p" + p + "c" + c + " div.back");
-                   var crd = remainingDeck.shift();
-                   f.html(crd.cardValue);
-                   f.attr("data-ordr", crd.card.order);
-                   f.attr("data-suit", crd.suit.name);
-                   f.attr("data-value", crd.card.value);
-                   f.css( "color", crd.suit.color);
-                   if( howToDealThisRound == "up" || p==0 ){
-                       b.css({transform: "rotateY(180deg)"});
-                   }else{
-                       f.css({transform: "rotateY(180deg)"});
-                   }
-               }
-               playerCardCount++;
-           }
-       }
-       if( dealToWhoThisRound == "community"){
-           // find the first unplayed card in the hand
-           var nextCardToPlay = this.findFirstUnplayedCard( "comm", cardsAlreadyDealt, cardsToDealThisRound );
-           for( var c = nextCardToPlay; c < cardsToDealThisRound; c++ ){
-               var f = $("#comm_c" + c + " div.front");
-               var b = $("#comm_c" + c + " div.back");
-               var crd = remainingDeck.shift();
-               f.html(crd.cardValue );
-               f.attr("data-ordr", crd.card.order);
-               f.attr("data-suit", crd.suit.name);
-               f.attr("data-value", crd.card.value);
-               if( howToDealThisRound == "up" || p==0 ){
-                   b.css({transform: "rotateY(180deg)"});
-               }else{
-                   f.css({transform: "rotateY(180deg)"});
-               }
-               communityCardCount++;
-           }
-       }
+
+       // deal the cards for this round
+       var dealTo = this.dealToFactory( dealToWhoThisRound );
+       cardCount = dealTo.dealCards( numberOfPlayers, cardCount, cardsToDealThisRound, howToDealThisRound, remainingDeck );
+
        round++;
-       cardsAlreadyDealt += cardsToDealThisRound;
+       cardCount.cards_already_dealt += cardsToDealThisRound;
        var pb = $("#play_button");
        if( round > Object.keys( gameData.dealOrder ).length  ){
-           var cardContainers =  $(".card-container") ? $(".card-container").length : 0;
+           var cardContainers =  $(".hand .card-container") ? $(".hand .card-container").length : 0;
            var numberOfHands = $(".hand") ? $(".hand").length : 1;
-           var parameters =  numberOfHands + "," + ( cardContainers / numberOfHands ) + "," + communityCardCount;
+           var parameters =  numberOfHands + "," + ( cardContainers / numberOfHands ) + "," + cardCount.community;
            pb.attr( "onclick", "gamePlay.reveal(" + parameters + ");" ).val( "Reveal?" );
        }else{
-           pb.attr( "onclick", "gamePlay.dealACard( "+ round +"," + cardsAlreadyDealt + " );" ).val( "Deal Round " + ( round ) );
+           var parameters = round + ',' + JSON.stringify( cardCount )
+           pb.attr( "onclick", 'gamePlay.dealACard( ' + parameters + ' );' ).val( "Deal Round " + ( round ) );
        }
        // and replace the "remainingDeck" on the page
        $("#deck").text( JSON.stringify(remainingDeck  ) );
    };
 
-   findFirstUnplayedCard( player, cards, cardsThisRound ){
-       var nextCardFound = false;
-       var nextCardToPlay = cards;
-       do{
-           var e = $("#" + player + "c" + nextCardToPlay + " div.front" );
-           if ( e ){
-               if ( !e.data() ){
-                   nextCardFound = true;
-               }else{
-                   nextCardToPlay++;
-               }
-           }
-       }while( nextCardFound === false && nextCardToPlay < cardsThisRound + cards );
-       return nextCardToPlay;
+
+   dealToFactory( type ){
+       var typesList = {
+           "players" : new DealToPlayers(),
+           "community" : new DealToCommunity(),
+           "replace" : new DealToReplace()
+       }
+       return typesList[ type ];
    }
-}
-
-class dealToPlayers{
-    dealCards(){
-
-    }
-}
-
-class dealToCommunity{
-    dealCards(){
-
-    }
-}
-
-class dealToReplace{
-    dealCards(){
-
-    }
 }
